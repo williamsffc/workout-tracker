5b08ca5755f69fe231366ba98ca1c2e738f5385d
William Flores <williamsffc@gmail.com>
2020-09-16 07:47:08 -0700

    linking htmls - app.get

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,9 +15,20 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+const databaseUrl = "fitness";
+const collections = ["workout"];
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
     useNewUrlParser: true, 
     useUnifiedTopology: true
+});
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/stats.html"));
 });
 
 //routes
@@ -26,3 +38,5 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
 });
+
+// https://git.heroku.com/murmuring-wildwood-71245.git
